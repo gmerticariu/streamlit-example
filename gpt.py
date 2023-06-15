f393ac39e1c2a6c5b74904d3d2b38a983a95753d
George Merticariu <george.merticariu@snowflake.com>
2023-06-15 17:20:12 +0300

    no matplot

--- gpt.py
+++ gpt.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 
 # Set up the Streamlit layout
 st.set_page_config(layout="wide")
@@ -9,15 +7,13 @@ st.title("Slider Bar Chart App")
 # Create a slider
 slider_value = st.slider("Select a value", min_value=1, max_value=10, value=5, step=1)
 
-# Generate random data for the bar chart
-x = np.arange(1, 11)
-y = np.random.randint(1, 10, size=(10,))
-
-# Update the value in the bar chart based on the slider
-y[slider_value-1] = slider_value
+# Generate bar chart data
+x = list(range(1, 11))
+y = [1] * 10
+y[slider_value - 1] = slider_value
 
 # Create the bar chart
-fig, ax = plt.subplots()
+fig, ax = st.pyplot()
 ax.bar(x, y)
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
@@ -25,3 +21,4 @@ ax.set_title("Bar Chart")
 
 # Display the bar chart using Streamlit
 st.pyplot(fig)
+
